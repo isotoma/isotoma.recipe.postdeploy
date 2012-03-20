bb63b2c12574c740e76966e35a512dba661c4c12
John Carr <john.carr@unrouted.co.uk>
2012-03-20 17:03:52 +0000

    Whitespace

--- isotoma/recipe/postdeploy/history.py
+++ isotoma/recipe/postdeploy/history.py
@@ -8,8 +8,8 @@ def get_values_removed_from_lists(path, buildout, keys):
     for key in keys:
         p, k = key.split(":")
 
-	# Get the current buildout state and combine it with the previous state
-	# so we know about all items that were ever in the list
+        # Get the current buildout state and combine it with the previous state
+        # so we know about all items that were ever in the list
         current = set(buildout[p].get_list(k))
         old = set(current)
         if store.has_key(key):
@@ -17,9 +17,9 @@ def get_values_removed_from_lists(path, buildout, keys):
 
         store[key] = old
 
-	# Things that used to be in the list but are no longer. Because we use
-	# sets we lost ordering, so we do an alpha sort so that consumers of
-	# this data at least get a stable ordering.
+        # Things that used to be in the list but are no longer. Because we use
+        # sets we lost ordering, so we do an alpha sort so that consumers of
+        # this data at least get a stable ordering.
         just_removed = list(old - current)
         just_removed.sort()
 
